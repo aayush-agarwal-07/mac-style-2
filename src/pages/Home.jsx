a8808135a79d355088da51885eed9363527b9401
Aayush K Agarwal <aayushagarwal685@gmail.com>
2025-11-30 02:15:29 +0530

    minor UI Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,39 +10,29 @@ export default function Home() {
         {/* <h2 className="header">My Work</h2> */}
         <Header />
         <HeroCta />
-        {/* RIGHT */}
-        <section className="project-right about-me-msite">
-          <header className="project-title">
-            <h1 className="project-heading">Kunal Rastogi</h1>
-            {/* No subtitle variable used */}
-          </header>
-
-          <div className="project-sections">
-            <article className="doc-section">
-              <p className="section-text">
-                Highly skilled and accomplished Senior Visual Designer with a
-                passion for creating exceptional brand experiences. With +6
-                years of experience in the (D2C) industry and a successful
-                freelance career working with diverse brands, I bring a unique
-                blend of creativity, leadership, and expertise to every project.
-                Known for mentoring and guiding teams to achieve their full
-                potential, I am now seeking a challenging role as a Design
-                Manager to contribute to the success of an organization.
-              </p>
-              {/* <p className="section-text">
-                Highly skilled and accomplished Senior Visual Designer with a
-                passion for creating exceptional brand experiences. With +6
-                years of experience in the (D2C) industry and a successful
-                freelance career working with diverse brands, I bring a unique
-                blend of creativity, leadership, and expertise to every project.
-                Known for mentoring and guiding teams to achieve their full
-                potential, I am now seeking a challenging role as a Design
-                Manager to contribute to the success of an organization.
-              </p> */}
-            </article>
-          </div>
-        </section>
       </main>
+      {/* RIGHT */}
+      <section className="project-right about-me-msite">
+        <header className="project-title">
+          <h1 className="project-heading">Kunal Rastogi</h1>
+          {/* No subtitle variable used */}
+        </header>
+
+        <div className="project-sections">
+          <article className="doc-section">
+            <p className="section-text">
+              Highly skilled and accomplished Senior Visual Designer with a
+              passion for creating exceptional brand experiences. With +6 years
+              of experience in the (D2C) industry and a successful freelance
+              career working with diverse brands, I bring a unique blend of
+              creativity, leadership, and expertise to every project. Known for
+              mentoring and guiding teams to achieve their full potential, I am
+              now seeking a challenging role as a Design Manager to contribute
+              to the success of an organization.
+            </p>
+          </article>
+        </div>
+      </section>
     </>
   );
 }
