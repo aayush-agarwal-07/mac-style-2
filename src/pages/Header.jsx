bc8ecff990fddda26578927a8fe5cd2088df39e6
Aayush K Agarwal <aayushagarwal685@gmail.com>
2025-11-29 20:26:44 +0530

    minor UI Changes

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,12 +2,16 @@
 import { Link } from "react-router-dom";
 import "./../styles/layout.css";
 
-export default function Header(){
+export default function Header() {
   return (
     <header className="site-header">
-      <div className="brand">Kunal Rastogi,</div>
+      <Link to="/" className="brand">
+        Kunal Rastogi,
+      </Link>
       <nav>
-        <Link to="/about" className="about">About me</Link>
+        <Link to="/about" className="about">
+          About me
+        </Link>
       </nav>
     </header>
   );
